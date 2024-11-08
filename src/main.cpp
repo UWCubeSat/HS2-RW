fbc45f87ecaa1bfe21ca1098bd58196f0706faae
darudecookie <darudecookie@gmail.com>
2024-11-08 12:18:06 -0800

    added code comments and test variable classes to hopefully make testing easier

--- src/main.cpp
+++ src/main.cpp
@@ -23,19 +23,20 @@ static void SetupSd();
 static void SetupRpm();
 
 rw_status::RwStatus wheel_status;
-controller::QuaternionPD qpd(1, 0);
-// controller::WheelSpeedPD init_wpd(1e-2, 0);
-controller::WheelSpeedPD wpd(1e-3, 0);
-pointing_modes::FourWheelMode four;
+controller::QuaternionPD QuaternionTorque_PD(test_parameters::torque_PD_params[0], test_parameters::torque_PD_params[1]);
+// controller::WheelSpeedPD init_WheelSpeed_PD(1e-2, 0);
+controller::WheelSpeedPD WheelSpeed_PD(test_parameters::wheel_speed_PD_params[0], test_parameters::wheel_speed_PD_params[1]);
+pointing_modes::FourWheelMode WheelController;
 
 /* Loop functions */
-// Itializes system time
+// Initializes system time
 static void UpdateSysTime();
 // Reads the latest information from the imu.
 // Returns the attitude quaternion reading in q and angular velocity in v.
-static void ReadImu(imu::Quaternion& q, imu::Vector<3>& v);
+static void ReadImu(imu::Quaternion &q, imu::Vector<3> &v);
 
-void setup() {
+void setup()
+{
   SetupSerial();
   SetupMotors();
   SetupImu();
@@ -47,46 +48,49 @@ void setup() {
 }
 
 int counter = 0;
-void loop() {
+void loop()
+{
   UpdateSysTime();
 
-  imu::Quaternion q;
-  imu::Vector<3> v;
-  ReadImu(q, v);
+  imu::Quaternion current_quaternion;
+  imu::Vector<3> current_gyro_reading;
+  ReadImu(current_quaternion, current_gyro_reading);
 
-  imu::Quaternion q_desired(0.0, 0.0, 0.0, 1.0);
-  imu::Vector<3> torque_req = qpd.Compute(q_desired, q, v);
+  imu::Vector<3> torque_req = QuaternionTorque_PD.Compute(test_parameters::test_target_quaternion, current_quaternion, current_gyro_reading);
   float wheel_torques[4];
   uint8_t pwm[4];
   Serial.print("q: ");
-  imu::Quaternion qe = q_desired.conjugate() * q;
-  Serial.print(q.w());
+  imu::Quaternion qe = test_parameters::test_target_quaternion.conjugate() * current_quaternion;
+  Serial.print(current_quaternion.w());
   Serial.print(", ");
-  Serial.print(q.x());
+  Serial.print(current_quaternion.x());
   Serial.print(", ");
-  Serial.print(q.y());
+  Serial.print(current_quaternion.y());
   Serial.print(", ");
-  Serial.println(q.z());
+  Serial.println(current_quaternion.z());
   // Serial.print("wheel_torques: ");
   // Serial.println(wheel_torques[0]);
   // Serial.print("dt: ");
   // Serial.println(timer::loop_dt);
   // Serial.print("calc: ");
-  // Serial.println(wheel_torques[0] / four.kWheelMoment[0] * timer::loop_dt);
+  // Serial.println(wheel_torques[0] / WheelController.kWheelMoment[0] * timer::loop_dt);
   // Serial.println(interrupt::wheel_rpm[0]);
 
-  if (counter < 200) {
-    float test[] = {15000, 15000, 15000, 15000};
-    four.Test_Speed_Command(test, interrupt::wheel_rpm, timer::loop_dt, wpd, pwm);
-    counter++;
+  // if (counter < test_parameters::spin_up_ticks) {
+  if (timer::init_time / 1000.f < test_parameters::spin_up_seconds)
+  {
+    WheelController.Test_Speed_Command(test_parameters::test_target_speed, interrupt::wheel_rpm, timer::loop_dt, WheelSpeed_PD, pwm);
+    //counter++;
     Serial.println("init speeds");
-  } else {
-    four.Calculate(torque_req, wheel_torques);
-    four.Pid_Speed(wheel_torques, timer::loop_dt, wpd, interrupt::wheel_rpm, pwm);
+  }
+  else
+  {
+    WheelController.Calculate(torque_req, wheel_torques);
+    WheelController.Pid_Speed(wheel_torques, timer::loop_dt, WheelSpeed_PD, interrupt::wheel_rpm, pwm);
   }
 
-
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++)
+  {
     digitalWrite(physical::kDirectionPins[i], 1);
     analogWrite(physical::kPwmPins[i], abs(pwm[i]));
   }
@@ -119,62 +123,78 @@ void loop() {
 }
 
 /* Setup */
-static void SetupSerial() {
+static void SetupSerial()
+{
   Serial.begin(physical::kSerialRate);
-  while (!Serial) {}  // wait for Serial
+  while (!Serial)
+  {
+  } // wait for Serial
 }
-static void SetupMotors() {
+static void SetupMotors()
+{
   // init motor pins
-  for (int i = 0; i < physical::kNumWheels; i++) {
+  for (int i = 0; i < physical::kNumWheels; i++)
+  {
     pinMode(physical::kPwmPins[i], OUTPUT);
     pinMode(physical::kDirectionPins[i], OUTPUT);
   }
 }
-static void SetupImu() {
+static void SetupImu()
+{
   // TODO anything here failing is pretty bad. It would be impossible for both
   // reaction wheels and magnetorquers to have functionality.
-  if (!physical::bno.begin_I2C()) {
+  if (!physical::bno.begin_I2C())
+  {
     Serial.print("No BNO085 detected");
     exit(EXIT_FAILURE);
   }
   // GAME_ROTATION_VECTOR has no magnetometer input, so it's more applicable
-  // to HS3. Consider making it absolute orientation (respective to magentic
+  // to HS3. Consider making it absolute orientation (respective to magnetic
   // north) and doing math to get a relative orientation for satellites in a
   // magnetic field.
   // It gives values in quaternion form.
-  if (!physical::bno.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+  if (!physical::bno.enableReport(SH2_GAME_ROTATION_VECTOR))
+  {
     Serial.println("Could not enable game vector");
   }
-  // SH2_GYROSCOPE_CALIBRATED gives velocity for the x,y,z axies.
+  // SH2_GYROSCOPE_CALIBRATED gives velocity for the x,y,z axes.
   // It includes a bias for compensation that can be separated with
   // SH2_GYROSCOPE_UNCALIBRATED
-  if (!physical::bno.enableReport(SH2_GYROSCOPE_CALIBRATED)) {
+  if (!physical::bno.enableReport(SH2_GYROSCOPE_CALIBRATED))
+  {
     Serial.println("Could not enable game vector");
   }
 }
-static void SetupSd() {
+static void SetupSd()
+{
   // init SD reader
   pinMode(physical::kSdPin, OUTPUT);
   digitalWrite(physical::kSdPin, HIGH);
   pinMode(SS, OUTPUT);
-  if (!SD.begin(physical::kSdPin)) {
+  if (!SD.begin(physical::kSdPin))
+  {
     Serial.println("card failed or not present");
     // don't do anything more:
     return;
   }
   Serial.println("card initialized.");
   physical::file = SD.open("data.csv", FILE_WRITE);
-  if (physical::file) {
+  if (physical::file)
+  {
     physical::file.println("setpoint,rpm0,error,send pwm");
-  } else {
+  }
+  else
+  {
     Serial.println("card failed write");
     physical::file.close();
     exit(EXIT_FAILURE);
   }
 }
-static void SetupRpm() {
-  for (int i = 0; i < physical::kNumWheels; i++) {
-  pinMode(physical::kFgPins[i], INPUT);
+static void SetupRpm()
+{
+  for (int i = 0; i < physical::kNumWheels; i++)
+  {
+    pinMode(physical::kFgPins[i], INPUT);
   }
   attachInterrupt(digitalPinToInterrupt(physical::kFgPins[0]), interrupt::ReadRpm0, FALLING);
   attachInterrupt(digitalPinToInterrupt(physical::kFgPins[1]), interrupt::ReadRpm1, FALLING);
@@ -183,39 +203,49 @@ static void SetupRpm() {
 
   // RPM timers
   // wheels start at 0, which relative to internal is this
-  for (int i = 0; i < physical::kNumWheels; i++) { interrupt::wheel_rpm[i] = -interrupt::kZeroRpm; }
+  for (int i = 0; i < physical::kNumWheels; i++)
+  {
+    interrupt::wheel_rpm[i] = -interrupt::kZeroRpm;
+  }
 
   // setup global timer
   interrupt::global_time = 0;
   ITimer3.init();
-  if (ITimer3.attachInterrupt(interrupt::kGlobalRate, interrupt::TimerHandler)) {
+  if (ITimer3.attachInterrupt(interrupt::kGlobalRate, interrupt::TimerHandler))
+  {
     Serial.println("Starting ITimer3 OK, millis() = " + String(millis()));
-  } else {
+  }
+  else
+  {
     Serial.println("Can't set ITimer3. Select another freq. or timer");
   }
 }
 
 /* Loop */
-static void UpdateSysTime() {
+static void UpdateSysTime()
+{
   timer::loop_prev_start_time = timer::loop_start_time;
   timer::loop_start_time = millis();
   timer::loop_dt = timer::loop_start_time - timer::loop_prev_start_time;
 }
-static void ReadImu(imu::Quaternion& q, imu::Vector<3>& v) {
+static void ReadImu(imu::Quaternion &q, imu::Vector<3> &v)
+{
   sh2_SensorValue_t sensor_value;
-  if(!physical::bno.getSensorEvent(&sensor_value)) {
+  if (!physical::bno.getSensorEvent(&sensor_value))
+  {
     Serial.println("bno085 not responsive");
   }
 
-  switch (sensor_value.sensorId) {
-    case SH2_GAME_ROTATION_VECTOR:
-      q = {sensor_value.un.gameRotationVector.real,
-        sensor_value.un.gameRotationVector.i,
-        sensor_value.un.gameRotationVector.j,
-        sensor_value.un.gameRotationVector.k};
-      break;
-    case SH2_GYROSCOPE_CALIBRATED:
-      v = {sensor_value.un.gyroscope.x, sensor_value.un.gyroscope.y,
-        sensor_value.un.gyroscope.z};
+  switch (sensor_value.sensorId)
+  {
+  case SH2_GAME_ROTATION_VECTOR:
+    q = {sensor_value.un.gameRotationVector.real,
+         sensor_value.un.gameRotationVector.i,
+         sensor_value.un.gameRotationVector.j,
+         sensor_value.un.gameRotationVector.k};
+    break;
+  case SH2_GYROSCOPE_CALIBRATED:
+    v = {sensor_value.un.gyroscope.x, sensor_value.un.gyroscope.y,
+         sensor_value.un.gyroscope.z};
   }
 }
--- src/main.hpp
+++ src/main.hpp
@@ -7,41 +7,72 @@
 #include "SD.h"
 
 /* Physical Info */
-namespace physical {
-// physical constants
-static constexpr uint16_t kSerialRate = 9600;
-static constexpr uint8_t kNumWheels = 4;
+namespace physical
+{
+    // physical constants
+    static constexpr uint16_t kSerialRate = 9600;
+    static constexpr uint8_t kNumWheels = 4;
 
-// pins
-const uint8_t kPwmPins[kNumWheels] = {10, 11, 12, 13};
-const uint8_t kDirectionPins[kNumWheels] = {44, 40, 36, 32};
-const uint8_t kFgPins[kNumWheels] = {18, 19, 2, 3};  // rpm reading pins
+    // pins
+    const uint8_t kPwmPins[kNumWheels] = {10, 11, 12, 13};
+    const uint8_t kDirectionPins[kNumWheels] = {44, 40, 36, 32};
+    const uint8_t kFgPins[kNumWheels] = {18, 19, 2, 3}; // rpm reading pins
 
-// signals
-uint8_t pwm_signal[kNumWheels] = {0, 0, 0, 0};          // 0-255
-uint8_t direction_signal[kNumWheels] = {1, 1, 1, 1};    // 1 clockwise, 0 ccw
+    // signals
+    uint8_t pwm_signal[kNumWheels] = {0, 0, 0, 0};       // 0-255
+    uint8_t direction_signal[kNumWheels] = {1, 1, 1, 1}; // 1 clockwise, 0 ccw
 
-// imu
-Adafruit_BNO08x bno(-1);  // we use I2C for the IMU, so this is unncecessary
+    // imu
+    Adafruit_BNO08x bno(-1); // we use I2C for the IMU, so this is unnecessary
 
-// SD reader
-static constexpr uint8_t kChipSelect = 53;  // mega specific number
-static constexpr uint8_t kSdPin = 10;       // pin on the SD reader
-File file;                                  // file opened on the SD card
-}  // namespace physical
+    // SD reader
+    static constexpr uint8_t kChipSelect = 53; // mega specific number
+    static constexpr uint8_t kSdPin = 10;      // pin on the SD reader
+    File file;                                 // file opened on the SD card
+} // namespace physical
 
 /* Timing */
-namespace timer {
-uint8_t init_time;              // time in ms of finish setup
-uint32_t loop_start_time;       // time in ms of this loop
-uint32_t loop_prev_start_time;  // time in ms of prev loop
-uint32_t loop_dt;               // delta between current and prev loop
-}  // global time
+namespace timer
+{
+    uint8_t init_time;             // time in ms of finish setup
+    uint32_t loop_start_time;      // time in ms of this loop
+    uint32_t loop_prev_start_time; // time in ms of prev loop
+    uint32_t loop_dt;              // delta between current and prev loop
+} // global time
 
 /* Utility functions*/
-namespace util {
-// returns 1 if x > 0, 0 if x = 0, -1 if x < 0
-static inline bool sign(double x) { return (x < 0) ? -1 : ((x > 0) ? 1 : 0); }
+namespace util
+{
+    // returns 1 if x > 0, 0 if x = 0, -1 if x < 0
+    static inline bool sign(double x) { return (x < 0) ? -1 : ((x > 0) ? 1 : 0); }
 }
 
+/* configurable parameters for testing */
+
+namespace test_parameters
+{
+
+    const float torque_PD_params[] = {1.f, 0.f};
+    const float wheel_speed_PD_params[] = {1e-3, 0.f};
+
+    float test_target_speed[] = {15000.f, 15000.f, 15000.f, 15000.f};
+    imu::Quaternion test_target_quaternion(0.0, 0.0, 0.0, 1.0);
+
+    const int spin_up_ticks = 200;
+
+    const float spin_up_seconds = 5.f;
+
+
+    /*
+    so my understanding is the motors will spin up to the RPM's in 'test_target_speed', and then after 'spin_up_ticks', then they will try to orient themselves in the direction of 'test_target_quaternion'
+
+    I added 'spin_up_seconds', which replaces 'spin_up_ticks' by using the time since the program started instead of cycles, I also commented out the counter variable in main.cpp and added in one for 'spin_up_seconds', so that it works. My idea with this is that it might be easier to know what's going on with testing if we have a time instead of a counter.
+    200 ticks probably takes a lot less than a second, so I think the current value of 5 seconds for 'spin_up_seconds' is overkill but idk.
+
+    I also think that this is without the imu's magnetometer, meaning that the the initial rotation quaternion is wherever the system was pointing when it started.
+
+    If I understand this code I think I could try and implement some system that hits a series of quaternions in a row,  
+
+    */
+}
 #endif // RW_SRC_MAIN_HPP_
