be2b20bb7609aa7595af3a5344699ed24e784717
darudecookie <114975020+darudecookie@users.noreply.github.com>
2025-01-06 18:40:18 -0500

    added refactored code previously in alex-testing

---
+++ lib/Testing/testing.hpp
@@ -0,0 +1,55 @@
+#ifndef TESTING_HPP
+#define TESTING_HPP
+
+#include <../../include/quaternion.hpp>
+
+namespace test_parameters
+{
+
+    //----basic params----//
+
+    constexpr float test_delay_secs = 2.f; // time for program to wait after initiating to start testing
+    unsigned long timeout_secs = 5;   // time after 'test_delay_secs' the program will run before stopping and not reseting until hardware reset, so if the reaction wheel explodes it'll eventually stop spinning
+
+    int test_index = 0;      // index of current test
+    long test_init_time_ms = 0; // time current test started in millis
+
+    constexpr float torque_PD_params[2] = {1.f, 0.f};       // quaternion torque controller PD params
+    constexpr float wheel_speed_PD_params[2] = {1e-3, 0.f}; // wheel speed controller PD params
+
+    //----test params----//
+    struct individual_test // outline of the basic information that makes up a single test object
+    {
+        bool is_indefinite; // whether or not the current test should actually timeout
+        float delay_time_secs;   // time the test will take to timeout, if above is false
+
+        bool is_using_quaternion; // whether or not the test is using quaternion orientation control or RPM control
+        float test_value[4];      // quaternion or wheel RPM values
+    };
+
+    constexpr int number_of_tests = 4; // total number of tests
+
+    // data for each test, remember values are in the order of the 'individual_test' struct
+    individual_test list_of_tests[number_of_tests] = {
+        //      {is_indefinite, delay_time_secs, is_using_quaternion, test_value},
+        {false, 2.5, false, {500.f, 0, 0, 0}},
+        {false, 2.5, false, {100.f, 0, 0, 0}},
+        {false, 2.5, false, {1000.f, 0, 0, 0}},
+        {false, 2.5, false, {100.f, 0, 0, 0}},
+
+    };
+
+    //----printing and debugging----//
+    constexpr int cycles_per_print = 100; // number of loops to occur for a print statement, this is done because 'Serial' functions are pretty slow so we can reduce the amount of printing
+
+    constexpr bool print_current_quaternion = false;
+    constexpr bool print_target_quaternion = false;
+
+    constexpr bool print_current_RPM = false;
+    constexpr bool print_target_RPM = true;
+
+    constexpr bool print_current_PWM = false;
+
+    constexpr bool print_debug = false;
+} // namespace test_parameters
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "RwStatus.hpp"
 #include "Controller.hpp"
 #include "PointingModes.hpp"
+#include "testing.hpp"
 
 /* Setup functions */
 // Initializes serial monitor
@@ -22,159 +23,260 @@ static void SetupSd();
 // Initializes interrupt pins and timer
 static void SetupRpm();
 
+static void write_PWM(uint8_t PWMs[4]);
+// writes the pwm values to appropriate pins, defined out for readability
+
 rw_status::RwStatus wheel_status;
-controller::QuaternionPD qpd(1, 0);
-// controller::WheelSpeedPD init_wpd(1e-2, 0);
-controller::WheelSpeedPD wpd(1e-3, 0);
-pointing_modes::FourWheelMode four;
+controller::QuaternionPD QuaternionTorque_PD(test_parameters::torque_PD_params[0], test_parameters::torque_PD_params[1]);
+// controller::WheelSpeedPD init_WheelSpeed_PD(1e-2, 0);
+controller::WheelSpeedPD WheelSpeed_PD(test_parameters::wheel_speed_PD_params[0], test_parameters::wheel_speed_PD_params[1]);
+pointing_modes::FourWheelMode WheelController;
 
 /* Loop functions */
-// Itializes system time
+// Initializes system time
 static void UpdateSysTime();
 // Reads the latest information from the imu.
 // Returns the attitude quaternion reading in q and angular velocity in v.
-static void ReadImu(imu::Quaternion& q, imu::Vector<3>& v);
+static void ReadImu(imu::Quaternion &q, imu::Vector<3> &v);
 
-void setup() {
+static void print_float_array(float *array, int array_len, const char *array_identifier);
+
+long timeout_ms;
+void setup()
+{
   SetupSerial();
+  Serial.println("begin setup");
+
   SetupMotors();
+  /*
+  these two are commented out because currently testing only with arduino
   SetupImu();
   SetupSd();
+  */
   SetupRpm();
+  Serial.println("setup successful!");
+
+  Serial.println("starting testing delay");
+  delay((long)(test_parameters::test_delay_secs) * 1000);
+  Serial.println("testing delay over");
 
   timer::init_time = millis();
-  timer::loop_start_time = timer::init_time;
+  timer::current_loop_start_time = timer::init_time;
+  test_parameters::test_init_time_ms = timer::init_time;
+
+  timeout_ms = (test_parameters::timeout_secs * 1000) + timer::init_time;
 }
 
-int counter = 0;
-void loop() {
+bool should_serial = false;
+void loop()
+{
   UpdateSysTime();
 
-  imu::Quaternion q;
-  imu::Vector<3> v;
-  ReadImu(q, v);
+  if (timer::current_loop_start_time > ((test_parameters::timeout_secs * 1000) + timer::init_time))
+  {
+    // this completely halts the program and stops it
+    // the intent is that if a test goes wrong the test will stop on its own after an amount of time
+    Serial.println("test timeout exceeded");
+    Serial.flush();
 
-  imu::Quaternion q_desired(0.0, 0.0, 0.0, 1.0);
-  imu::Vector<3> torque_req = qpd.Compute(q_desired, q, v);
-  float wheel_torques[4];
-  uint8_t pwm[4];
-  Serial.print("q: ");
-  imu::Quaternion qe = q_desired.conjugate() * q;
-  Serial.print(q.w());
-  Serial.print(", ");
-  Serial.print(q.x());
-  Serial.print(", ");
-  Serial.print(q.y());
-  Serial.print(", ");
-  Serial.println(q.z());
-  // Serial.print("wheel_torques: ");
-  // Serial.println(wheel_torques[0]);
-  // Serial.print("dt: ");
-  // Serial.println(timer::loop_dt);
-  // Serial.print("calc: ");
-  // Serial.println(wheel_torques[0] / four.kWheelMoment[0] * timer::loop_dt);
-  // Serial.println(interrupt::wheel_rpm[0]);
-
-  if (counter < 200) {
-    float test[] = {15000, 15000, 15000, 15000};
-    four.Test_Speed_Command(test, interrupt::wheel_rpm, timer::loop_dt, wpd, pwm);
-    counter++;
-    Serial.println("init speeds");
-  } else {
-    four.Calculate(torque_req, wheel_torques);
-    four.Pid_Speed(wheel_torques, timer::loop_dt, wpd, interrupt::wheel_rpm, pwm);
+    exit(EXIT_SUCCESS);
   }
 
-
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(physical::kDirectionPins[i], 1);
-    analogWrite(physical::kPwmPins[i], abs(pwm[i]));
+  /*
+  This short block decides whether or not the current loop is one where values should be printed
+  This is its own block because different print functions need to occur at different places, and it saves having to do the calculation multiple different times.
+  ie if you want to print both target quaternion and target rpm, those don't exist at the same time so you need separate print blocks for each, and having the should_serial flag improves readability
+  */
+  if (timer::counter % test_parameters::cycles_per_print == 0)
+  {
+    should_serial = true;
+  }
+  else
+  {
+    should_serial = false;
   }
 
-  // TODO: use wheel_status
+  // The loop reads the IMU every cycle regardless of whether
+  imu::Quaternion current_quaternion;
+  imu::Vector<3> current_gyro_reading;
+  // ReadImu(current_quaternion, current_gyro_reading);- left commented out because currently testing only with arduino
 
-  // Serial.println(interrupt::wheel_rpm[0]);
-  // Serial.println(interrupt::wheel_rpm[1]);
-  // Serial.println(interrupt::wheel_rpm[2]);
-  // Serial.println(interrupt::wheel_rpm[3]);
-  // Serial.println(v[0]);
-  // Serial.println(v[1]);
-  // Serial.println(v[2]);
-  Serial.println(pwm[0]);
-  Serial.println(pwm[1]);
-  Serial.println(pwm[2]);
-  Serial.println(pwm[3]);
-  // Serial.println(torque_req[0]);
-  // Serial.println(torque_req[1]);
-  // Serial.println(torque_req[2]);
-  // Serial.println(wheel_torques[0]);
-  // Serial.println(wheel_torques[1]);
-  // Serial.println(wheel_torques[2]);
-  // Serial.println(wheel_torques[3]);
-  // Serial.println(q.w());
-  // Serial.println(q.x());
-  // Serial.println(q.y());
-  // Serial.println(q.z());
-  Serial.println("");
+  if (should_serial)
+  {
+    if (test_parameters::print_current_quaternion)
+    {
+      float quaternion_array[4] = {current_quaternion.w(), current_quaternion.x(), current_quaternion.y(), current_quaternion.z()};
+      print_float_array(quaternion_array, 4, "Current Quaternion");
+    }
+    if (test_parameters::print_current_RPM)
+    {
+      float rpm_array[4] = {interrupt::wheel_rpm[0], interrupt::wheel_rpm[1], interrupt::wheel_rpm[2], interrupt::wheel_rpm[3]};
+
+      print_float_array(rpm_array, sizeof(interrupt::wheel_rpm) / sizeof(interrupt::wheel_rpm[0]), "Current RPMs");
+    }
+  }
+  // imu::Quaternion qe = test_parameters::target_quaternion.conjugate() * current_quaternion;
+
+  // this block of code is responsible for the testing logic
+  if ((test_parameters::list_of_tests[test_parameters::test_index].is_indefinite == true) || (test_parameters::list_of_tests[test_parameters::test_index].delay_time_secs * 1000) > (timer::current_loop_start_time - test_parameters::test_init_time_ms))
+
+  // this if statement checks if either the current test's time hasn't elapsed or whether the current test has the indefinite value set to true, if yes to either the test changes
+  {
+    /*if a test is going on, this codeblock runs*/
+
+    uint8_t pwm_values[4] = {0, 0, 0, 0}; // creating pwm values
+
+    if (test_parameters::list_of_tests[test_parameters::test_index].is_using_quaternion == true)
+    // if test is quaternion control
+    {
+      // get target quaternion from list_of_tests
+      imu::Quaternion target_quaternion(test_parameters::list_of_tests[test_parameters::test_index].test_value[0], test_parameters::list_of_tests[test_parameters::test_index].test_value[1], test_parameters::list_of_tests[test_parameters::test_index].test_value[2], test_parameters::list_of_tests[test_parameters::test_index].test_value[3]);
+
+      // calculate required torque from previously calculated wheel torques
+      imu::Vector<3> required_torque = QuaternionTorque_PD.Compute(target_quaternion, current_quaternion, current_gyro_reading);
+
+      float required_wheel_torques[4];
+
+      WheelController.Calculate(required_torque, required_wheel_torques);
+      WheelController.Pid_Speed(required_wheel_torques, timer::loop_dt, WheelSpeed_PD, interrupt::wheel_rpm, pwm_values);
+
+      if (should_serial && test_parameters::print_target_quaternion)
+      {
+        print_float_array(test_parameters::list_of_tests[test_parameters::test_index].test_value, 4, "Target Quaternion");
+      }
+    }
+    else
+    {
+      // If not quaternion control, then it's rpm control
+
+      // Calculate wheel PWM's
+      WheelController.Test_Speed_Command(test_parameters::list_of_tests[test_parameters::test_index].test_value, interrupt::wheel_rpm, timer::loop_dt, WheelSpeed_PD, pwm_values);
+
+      if (should_serial && test_parameters::print_target_RPM)
+      {
+        print_float_array(test_parameters::list_of_tests[test_parameters::test_index].test_value, 4, "Target RPM");
+      }
+    }
+
+    write_PWM(pwm_values);
+
+    if (should_serial && test_parameters::print_current_PWM)
+    {
+      Serial.print("PWMs:");
+      for (int i = 0; i < 4; i++)
+      {
+        Serial.print(pwm_values[i]);
+        Serial.print(", ");
+      }
+      Serial.print("\n");
+    }
+  }
+  else
+  {
+    // if the current test is over, then either we advance to the next test, or there are no more tests, and the program is over
+
+    if (test_parameters::test_index < test_parameters::number_of_tests)
+    {
+      // if the current test is over, update the index to the next test, and set the next test start time as the current time
+
+      Serial.print("test ");
+      Serial.print(test_parameters::test_index);
+      Serial.print(" over, beginning with test ");
+
+      test_parameters::test_index++;
+      test_parameters::test_init_time_ms = timer::current_loop_start_time;
+
+      Serial.print(test_parameters::test_index);
+      Serial.println(" (0 indexed)");
+    }
+    else
+    {
+      // the program has no more tests, so after the last test times out, then the code will end up here, which is an empty block and nothing will happen
+    }
+  }
 }
 
 /* Setup */
-static void SetupSerial() {
+static void SetupSerial()
+{
   Serial.begin(physical::kSerialRate);
-  while (!Serial) {}  // wait for Serial
+  while (!Serial)
+  {
+    delay(10);
+  } // wait for Serial
 }
-static void SetupMotors() {
+static void SetupMotors()
+{
   // init motor pins
-  for (int i = 0; i < physical::kNumWheels; i++) {
+  for (int i = 0; i < physical::kNumWheels; i++)
+  {
     pinMode(physical::kPwmPins[i], OUTPUT);
+    digitalWrite(physical::kPwmPins[i], LOW);
+
     pinMode(physical::kDirectionPins[i], OUTPUT);
+    digitalWrite(physical::kDirectionPins[i], LOW);
   }
 }
-static void SetupImu() {
+static void SetupImu()
+{
   // TODO anything here failing is pretty bad. It would be impossible for both
   // reaction wheels and magnetorquers to have functionality.
-  if (!physical::bno.begin_I2C()) {
+  if (!physical::bno.begin_I2C())
+  {
     Serial.print("No BNO085 detected");
+    Serial.flush(); // flush here stops the message from not fully printing
+
     exit(EXIT_FAILURE);
   }
   // GAME_ROTATION_VECTOR has no magnetometer input, so it's more applicable
-  // to HS3. Consider making it absolute orientation (respective to magentic
+  // to HS3. Consider making it absolute orientation (respective to magnetic
   // north) and doing math to get a relative orientation for satellites in a
   // magnetic field.
   // It gives values in quaternion form.
-  if (!physical::bno.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+  if (!physical::bno.enableReport(SH2_GAME_ROTATION_VECTOR))
+  {
     Serial.println("Could not enable game vector");
   }
-  // SH2_GYROSCOPE_CALIBRATED gives velocity for the x,y,z axies.
+  // SH2_GYROSCOPE_CALIBRATED gives velocity for the x,y,z axes.
   // It includes a bias for compensation that can be separated with
   // SH2_GYROSCOPE_UNCALIBRATED
-  if (!physical::bno.enableReport(SH2_GYROSCOPE_CALIBRATED)) {
+  if (!physical::bno.enableReport(SH2_GYROSCOPE_CALIBRATED))
+  {
     Serial.println("Could not enable game vector");
   }
 }
-static void SetupSd() {
+static void SetupSd()
+{
   // init SD reader
   pinMode(physical::kSdPin, OUTPUT);
   digitalWrite(physical::kSdPin, HIGH);
   pinMode(SS, OUTPUT);
-  if (!SD.begin(physical::kSdPin)) {
+  if (!SD.begin(physical::kSdPin))
+  {
     Serial.println("card failed or not present");
     // don't do anything more:
     return;
   }
   Serial.println("card initialized.");
   physical::file = SD.open("data.csv", FILE_WRITE);
-  if (physical::file) {
+  if (physical::file)
+  {
     physical::file.println("setpoint,rpm0,error,send pwm");
-  } else {
+  }
+  else
+  {
     Serial.println("card failed write");
+    Serial.flush();
+
     physical::file.close();
     exit(EXIT_FAILURE);
   }
 }
-static void SetupRpm() {
-  for (int i = 0; i < physical::kNumWheels; i++) {
-  pinMode(physical::kFgPins[i], INPUT);
+static void SetupRpm()
+{
+  for (int i = 0; i < physical::kNumWheels; i++)
+  {
+    pinMode(physical::kFgPins[i], INPUT_PULLUP);
   }
   attachInterrupt(digitalPinToInterrupt(physical::kFgPins[0]), interrupt::ReadRpm0, FALLING);
   attachInterrupt(digitalPinToInterrupt(physical::kFgPins[1]), interrupt::ReadRpm1, FALLING);
@@ -183,39 +285,78 @@ static void SetupRpm() {
 
   // RPM timers
   // wheels start at 0, which relative to internal is this
-  for (int i = 0; i < physical::kNumWheels; i++) { interrupt::wheel_rpm[i] = -interrupt::kZeroRpm; }
+  for (int i = 0; i < physical::kNumWheels; i++)
+  {
+    interrupt::wheel_rpm[i] = -interrupt::kZeroRpm;
+  }
 
   // setup global timer
   interrupt::global_time = 0;
   ITimer3.init();
-  if (ITimer3.attachInterrupt(interrupt::kGlobalRate, interrupt::TimerHandler)) {
+  if (ITimer3.attachInterrupt(interrupt::kGlobalRate, interrupt::TimerHandler))
+  {
     Serial.println("Starting ITimer3 OK, millis() = " + String(millis()));
-  } else {
+  }
+  else
+  {
     Serial.println("Can't set ITimer3. Select another freq. or timer");
   }
 }
 
 /* Loop */
-static void UpdateSysTime() {
-  timer::loop_prev_start_time = timer::loop_start_time;
-  timer::loop_start_time = millis();
-  timer::loop_dt = timer::loop_start_time - timer::loop_prev_start_time;
+static void UpdateSysTime()
+{
+  timer::prev_loop_start_time = timer::current_loop_start_time;
+  timer::current_loop_start_time = millis();
+  timer::loop_dt = timer::current_loop_start_time - timer::prev_loop_start_time;
+  timer::counter++;
 }
-static void ReadImu(imu::Quaternion& q, imu::Vector<3>& v) {
+static void ReadImu(imu::Quaternion &q, imu::Vector<3> &v)
+{
+  /*
+  One pretty fundamental question I have here is that I'm pretty sure this function leaves either 'q' or 'v' in its default unassigned state.
+  In loop(), q and v are unassigned
+  */
+  // so these few lines are pretty self explanatory: create new sensor_value struct, and then read the imu, and if the read fails print an error
   sh2_SensorValue_t sensor_value;
-  if(!physical::bno.getSensorEvent(&sensor_value)) {
+  if (!physical::bno.getSensorEvent(&sensor_value))
+  {
     Serial.println("bno085 not responsive");
   }
 
-  switch (sensor_value.sensorId) {
-    case SH2_GAME_ROTATION_VECTOR:
-      q = {sensor_value.un.gameRotationVector.real,
-        sensor_value.un.gameRotationVector.i,
-        sensor_value.un.gameRotationVector.j,
-        sensor_value.un.gameRotationVector.k};
-      break;
-    case SH2_GYROSCOPE_CALIBRATED:
-      v = {sensor_value.un.gyroscope.x, sensor_value.un.gyroscope.y,
-        sensor_value.un.gyroscope.z};
+  // What I don't understand here the 'sensor_value.sensorId' can only be one number (right?), so EITHER case 1 will be true and q will be assigned OR case 2 will be true and v will be assigned
+  switch (sensor_value.sensorId)
+  {
+  case SH2_GAME_ROTATION_VECTOR:
+    q = {sensor_value.un.gameRotationVector.real,
+         sensor_value.un.gameRotationVector.i,
+         sensor_value.un.gameRotationVector.j,
+         sensor_value.un.gameRotationVector.k};
+    break;
+  case SH2_GYROSCOPE_CALIBRATED:
+    v = {sensor_value.un.gyroscope.x, sensor_value.un.gyroscope.y,
+         sensor_value.un.gyroscope.z};
+  }
+}
+static void write_PWM(uint8_t PWMs[4])
+{
+  for (int i = 0; i < 4; i++)
+  {
+    digitalWrite(physical::kDirectionPins[i], 1);
+    analogWrite(physical::kPwmPins[i], abs(PWMs[i]));
+  }
+}
+static void print_float_array(float *array, int array_len, const char *array_identifier)
+{
+  return;
+
+  Serial.print(array_identifier);
+  Serial.print(": ");
+
+  for (int i = 0; i < array_len; i++)
+  {
+    Serial.print(*(array + i));
+
+    (i == array_len - 1) ? Serial.println("") : Serial.print(", ");
   }
 }
